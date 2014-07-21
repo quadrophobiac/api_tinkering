8cb7fbdbeb047d09f094fd6390e6ee691c07c116
stephenfortune <stephen.j.fortune@gmail.com>
2014-07-21 21:23:45 +0100

    lots to resolve - a change of approach was necessitated by the fact that I do not have access to put composer on my trial hosted solution. Strange discrepancies abound, including non functionality in Chrome, certain php SDK pages breaking when hosted on laughingsquid but operating fine on localhost, and app _id keys not authenticating

--- php/fb-sdk/online.php
+++ php/fb-sdk/online.php
@@ -1,46 +1,50 @@
 <?php
-
+// uploaded to http://www.stephenfortune.net/projects/30sec/ as min.php
 // this uses the new SDK /facebook-php-sdk-v4
 // that SDK further compartmentalises the src files previously obtained
-// does not work on Chrome, unable to test it on other browsers because of web upload difficulty
+// WHAT DOES WORK
+// the importer can successfully import FacebookJavaScriptLoginHelper
+// WHAT DOESNT WORK
+// the secret and app key are not passing
+// all the requires commented out make the file break
+
+
 
 ini_set('error_reporting', E_ALL);
-//ini_set('display_errors', 'On'); 
+//ini_set('display_errors', 'On');
 // Must pass session data for the library to work
 // derived from http://www.benmarshall.me/facebook-sdk-php-v4/
-// FBRLH_state is nothing of note
+
 session_start();
 date_default_timezone_set('Europe/London');
 // Include the libraries files
 require_once( 'Facebook/FacebookRequest.php' );
-require_once( 'Facebook/FacebookResponse.php' );
 require_once( 'Facebook/FacebookSession.php' );
 require_once( 'Facebook/Entities/AccessToken.php' );
 require_once( 'Facebook/Entities/SignedRequest.php' ); // added to assuage FacebookSignedRequestFromInputHelper error
-require_once( 'Facebook/GraphNodes/GraphObject.php' );
-require_once( 'Facebook/GraphNodes/GraphSessionInfo.php' );
 require_once( 'Facebook/HttpClients/FacebookCurl.php' );
 require_once( 'Facebook/HttpClients/FacebookHttpClientInterface.php' );
-//require_once( 'Facebook/HttpClients/FacebookHttpable.php' ); FacebookHttpClientInterface
 require_once( 'Facebook/HttpClients/FacebookCurlHttpClient.php' );
 require_once( 'Facebook/Exceptions/FacebookSDKException.php' );
 require_once( 'Facebook/Exceptions/FacebookRequestException.php' );
 require_once( 'Facebook/Exceptions/FacebookAuthorizationException.php' );
 require_once( 'Facebook/Helpers/FacebookRedirectLoginHelper.php' );
- // requires FacebookSignedRequestFromInputHelper also apparently
-require_once( 'Facebook/Helpers/FacebookSignedRequestFromInputHelper.php'); // if added before JS loginHelper, no error thrown
-
+require_once( 'Facebook/Helpers/FacebookSignedRequestFromInputHelper.php');
 require_once( 'Facebook/Helpers/FacebookJavaScriptLoginHelper.php' ); // <-- the problem
 
+//require_once( 'Facebook/GraphNodes/GraphObject.php' );
+//require_once( 'Facebook/GraphNodes/GraphSessionInfo.php' );
+//require_once( 'Facebook/FacebookResponse.php' ); // breaks the page
+
+
 use Facebook\FacebookRequest;
-use Facebook\FacebookResponse;
 use Facebook\FacebookSession;
 use Facebook\Entities\AccessToken; // adding entities to alias doesn't hinder it
 use Facebook\Entities\SignedRequest;
-use Facebook\GraphNodes\GraphSessionInfo;
-use Facebook\GraphNodes\GraphObject;
+// use Facebook\GraphNodes\GraphSessionInfo;
+// use Facebook\GraphNodes\GraphObject;
 use Facebook\HttpClients\FacebookCurl; // adding HttpClients to alias doesn't hinder it
-//use Facebook\HttpClients\FacebookHttpable; // adding HttpClients to alias doesn't hinder it
+// //use Facebook\HttpClients\FacebookHttpable; // adding HttpClients to alias doesn't hinder it
 use Facebook\HttpClients\FacebookHttpClientInterface;
 use Facebook\HttpClients\FacebookCurlHttpClient; // adding HttpClients to alias doesn't hinder it
 use Facebook\Exceptions\FacebookAuthorizationException;
@@ -51,9 +55,13 @@ use Facebook\Helpers\FacebookSignedRequestFromInputHelper;
 
 use Facebook\Helpers\FacebookJavaScriptLoginHelper; // <-- the problem
 
+// dependancies above == fine!
+
+// use Facebook\FacebookResponse;
+
 // Replace the APP_ID and APP_SECRET with your apps credentials
 FacebookSession::setDefaultApplication( 'APP_ID','APP_SECRET' ); // 'APP_ID','APP_SECRET'
-echo '<a href="http://localhost/api_tinkering/php/fb-sdk/logout.php?dest=min">LogOut</a><br>';
+echo '<a href="http://www.stephenfortune.net/projects/30sec/logout.php?dest=min">LogOut</a><br>';
 
 // Create the login helper and replace REDIRECT_URI with your URL
 // Use the same domain you set for the apps 'App Domains'
@@ -71,48 +79,51 @@ try {
   // When validation fails or other local issues
   echo "other exception ".$ex;
 }
+// nothing is found for above while the key and secret are problematic
+
+//     if ( isset( $session ) ) {
+//        print("session<br>\n");
+
+//        // Save the session
+//        $_SESSION['fb_token'] = $session->getToken();
+
+//        // Create session using saved token or the new one we generated at login
+//        $session = new FacebookSession( $session->getToken() );
+//        echo "testing session assignment from FacebookSession constructor<br>";
+//        echo print_r($session);
+// //       // why reassign breaks when it saves
+// //       // return (string) $session->accessToken; <- dont know whrere this came from!
+//        fbData($session);
+//      } else {
+// //       // No session
+//      print("<br>no session<br>\n");
+//      echo var_dump($session);
+//        // Requested permissions - optional, If no permissions are provided, it’ll use Facebook’s default public_profile 
+//        // $permissions = array(
+//        //   'email',
+//        //   'user_location',
+//        //   'user_birthday'
+//        // );
+//      echo "<br>";
+//      // uncomment below depending on redirect or JS helper
+//        // $loginURL = $helper->getLoginUrl(); // if passing scope vars; getLoginUrl($permissions);
+//        // echo '<br><a href="' . $loginURL . '">The Real Login</a><br>' .$session;
+//      }
+//      echo '<br><a href="http://localhost/api_tinkering/php/fb-sdk/min.php">Home</a><br>';
+
+//      function fbData($session){
+//          //print some FB data
+//          $request = (new FacebookRequest( $session, 'GET', '/me' ))->execute();
+//          // Get response as an array
+//          $user = $request->getGraphObject()->asArray();
+//          foreach ($user as $e) {
+//            echo($e."<br>");
+//          }
+//      }
 
 
-    if ( isset( $session ) ) {
-      print("session<br>\n");
-
-      // Save the session
-      $_SESSION['fb_token'] = $session->getToken();
-
-      // Create session using saved token or the new one we generated at login
-      $session = new FacebookSession( $session->getToken() );
-      echo "testing session assignment from FacebookSession constructor<br>";
-      echo print_r($session);
-      // why reassign breaks when it saves
-      // return (string) $session->accessToken; <- dont know whrere this came from!
-      fbData($session);
-    } else {
-      // No session
-    print("<br>no session<br>\n");
-    echo var_dump($session);
-      // Requested permissions - optional, If no permissions are provided, it’ll use Facebook’s default public_profile 
-      // $permissions = array(
-      //   'email',
-      //   'user_location',
-      //   'user_birthday'
-      // );
-    echo "<br>";
-    // uncomment below depending on redirect or JS helper
-      // $loginURL = $helper->getLoginUrl(); // if passing scope vars; getLoginUrl($permissions);
-      // echo '<br><a href="' . $loginURL . '">The Real Login</a><br>' .$session;
-    }
-    echo '<br><a href="http://localhost/api_tinkering/php/fb-sdk/min.php">Home</a><br>';
-
-    function fbData($session){
-        //print some FB data
-        $request = (new FacebookRequest( $session, 'GET', '/me' ))->execute();
-        // Get response as an array
-        $user = $request->getGraphObject()->asArray();
-        foreach ($user as $e) {
-          echo($e."<br>");
-        }
-    }
 print "<br>end PHP <br>";
+
 ?>
 <html>
   <head>
